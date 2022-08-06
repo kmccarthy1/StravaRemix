2b8f838860b2a48dd9696b55687c9b7fb379574f
Kevin McCarthy <km.mccarthy11@gmail.com>
2022-08-05 22:28:44 -0400

    succesful api connection

--- stravaremix/src/App.tsx
+++ stravaremix/src/App.tsx
@@ -1,26 +1,38 @@
-import React, {useEffect} from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import './App.css';
-import axios from 'axios';
+import React, {useEffect, useState} from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+
+
+  const [activities, setActivities] = useState<Node[]>([]);
+
 
   const clientID = '91367';
   const clientSecret = 'fdca657660112b4e0a8c3ef32bbb86bebae9db5e';
   const refreshToken = '160616ca8b0d2d8a6f19acf5f5c6608e7bbba24c';
-  const activities_link = 'https://www.strava.com/api/v3/athlete/activities'
+  const auth_link = 'https://www.strava.com/oauth/token';
+  const activities_link = 'https://www.strava.com/api/v3/athlete/activities';
 
 
   useEffect(() => {
+
     async function fetchData() {
-      const stravaAuthResponse = await axios.get(`${activities_link}?access_token=62efb3c78eab7f60f5bf84828489157dde82db46`)
-      console.log(stravaAuthResponse);
+
+      const stravaAuthResponse = await axios.all([
+        axios.post(`${auth_link}?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`)
+      ]);
+      const stravaActivityResponse = await axios.get(`${activities_link}?access_token=${stravaAuthResponse[0].data.access_token}`);
+      console.log(stravaActivityResponse.data[0].name);
+      setActivities(stravaActivityResponse.data[0].name)
     }
 
     fetchData();
   }, []);
   return (
-    <h1>testing</h1>
+    <h2>test</h2>
+    <h1></h1>
 /* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
   <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
